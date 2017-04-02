8e4433c1714553f7f9d3442103c0ebe7455fe7e3
Rhoulhac <greg.rhoulhac@gmail.com>
2017-04-02 08:25:23 -0500

    Adding links for current and previous monthly spending

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 # from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -11,5 +13,39 @@ class CategoryListView(ListView):
 
 class TransactionListView(ListView):
     model = mymodels.Transaction
-    paginate_by = 10
+    # paginate_by = 10
     ordering = '-purchase_date'
+
+
+class CurrentMonthAllListView(TransactionListView):
+
+    def get_queryset(self):
+        """ Show only Transactions for the current month """
+        return mymodels.Transaction.objects.filter(
+            purchase_date__year=datetime.datetime.now().year,
+            purchase_date__month=datetime.datetime.now().month
+        )
+
+    def get_context_data(self, **kwargs):
+        """ Transform queryset """
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['current'] = 'current'
+        return kwargs
+
+
+class PreviousMonthAllListView(TransactionListView):
+
+    def get_queryset(self):
+        """ Show only Transactions for the previous month """
+        return mymodels.Transaction.objects.filter(
+            purchase_date__year=datetime.datetime.now().year,
+            purchase_date__month=datetime.datetime.now().month - 1
+        )
+
+    def get_context_data(self, **kwargs):
+        """ Transform queryset """
+        kwargs = super().get_context_data(**kwargs)
+        catg = mymodels.Category.objects.all()
+        kwargs['previous'] = 'previous'
+        kwargs['categories'] = catg
+        return kwargs
