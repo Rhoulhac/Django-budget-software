8a726239ee86f4a6b1da0f299bfdfd0b7350257d
Rhoulhac <greg.rhoulhac@gmail.com>
2017-06-26 22:33:22 -0500

    Set up progress bar for budgeting, add filters on Admin, add sorting for models,

--- budget/models.py
+++ budget/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField("Store", unique=True, max_length=255)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
@@ -29,54 +32,6 @@ class PaymentMethod(models.Model):
         return self.name
 
 
-class TransactionQueryset(models.QuerySet):
-
-    def current_monthly_spending(self):
-        monthly_list = []
-        query = self.filter(
-            purchase_date__year=datetime.datetime.now().year,
-            purchase_date__month=datetime.datetime.now().month
-        )
-        for q in query:
-            monthly_list.append({
-                'purchase_date': q.purchase_date,
-                'amount': q.amount,
-                'store': q.store,
-                'category': q.category,
-                'payment_method': q.payment_method,
-            })
-        return monthly_list
-
-    def previous_monthly_spending(self):
-        prev_monthly_list = []
-        query = self.filter(
-            purchase_date__year=datetime.datetime.now().year,
-            purchase_date__month=datetime.datetime.now().month - 1
-        )
-        for q in query:
-            prev_monthly_list.append({
-                'purchase_date': q.purchase_date,
-                'amount': q.amount,
-                'store': q.store,
-                'category': q.category,
-                'payment_method': q.payment_method,
-            })
-        return prev_monthly_list
-
-    def previous_years_monthly_spending(self):
-        return self.filter(
-            purchase_date__year=datetime.datetime.now().year - 1,
-            purchase_date__month=datetime.datetime.now().month
-        )
-
-    def current_monthly_spending_by_category(self, category):
-        return self.filter(
-            category=category,
-            purchase_date__year=datetime.datetime.now().year,
-            purchase_date__month=datetime.datetime.now().month
-        )
-
-
 class Transaction(models.Model):
     store = models.ForeignKey(Store)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
@@ -84,11 +39,8 @@ class Transaction(models.Model):
     category = models.ForeignKey(Category)
     payment_method = models.ForeignKey(PaymentMethod)
 
-    objects = TransactionQueryset().as_manager()
-
-    def last_year_monthly(self):
-        pass
-
+    class Meta:
+        ordering = ['-purchase_date']
 
 class Savings(models.Model):
     name = models.CharField("Savings Category", unique=True, max_length=255)
