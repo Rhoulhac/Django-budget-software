a52b0ed7259b08485354a76b94480d4ded2d5d79
Rhoulhac <greg.rhoulhac@gmail.com>
2018-02-23 23:50:43 -0600

    Add Last Month filter to admin for purchase date

--- budget/admin.py
+++ budget/admin.py
@@ -1,5 +1,45 @@
+import datetime
+
 from django.contrib import admin
+from django.contrib.admin.filters import DateFieldListFilter
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 from .models import Store, Transaction, Category, Savings, PaymentMethod
+
+
+class MyDateTimeFilter(DateFieldListFilter):
+    def __init__(self, *args, **kwargs):
+        super(MyDateTimeFilter, self).__init__(*args, **kwargs)
+
+        now = timezone.now()
+        if timezone.is_aware(now):
+            now = timezone.localtime(now)
+
+        today = now.date()
+
+        if today.month == 1:
+            last_month = today.replace(year=today.year - 1, month=12, day=1)
+        else:
+            last_month = today.replace(month=today.month - 1, day=1)
+
+        # Remove unwanted links
+        list_links = list(self.links)
+        for item in list_links:
+            if item[0] in ('Past 7 days', 'No date', 'Has date'):
+                list_links.remove(item)
+        # Last list item not removed...weird
+        if list_links[-1][0] == 'Has date':
+            list_links.remove(list_links[-1])
+        self.links = tuple(list_links)
+
+        self.links += ((
+            (_('Last Month'), {
+                self.lookup_kwarg_since: str(last_month),
+                self.lookup_kwarg_until: str(today.replace(day=1)),
+            }),
+        ))
+
 
 admin.site.register(Store)
 admin.site.register(PaymentMethod)
@@ -10,8 +50,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('purchase_date_and_store', 'category', 'amount', 'payment_method')
-    list_filter = ('category', 'store', 'payment_method')
+    list_display = ('purchase_date_and_store', 'category', 'amount', 'payment_method', 'notes')
+    list_filter = ('category', ('purchase_date', MyDateTimeFilter), 'payment_method', 'store')
 
     def purchase_date_and_store(self, obj):
         return f'{obj.purchase_date.strftime("%b %d")} - {obj.store}'
