0d0e728abf6f042589129fc101ec95e7f640ad6e
Rhoulhac <greg.rhoulhac@gmail.com>
2018-02-23 21:21:59 -0600

    URL updates

--- budget/urls.py
+++ budget/urls.py
@@ -1,26 +1,15 @@
 from django.conf.urls import url
+from django.urls import path
 
 from .views import CategoryListView, TransactionListView, CurrentMonthAllListView, \
     PreviousMonthAllListView, ThreePreviousMonthsAllListView
 
 
 urlpatterns = [
-    url(r'', TransactionListView.as_view()),
-    url(r'^categories/$', CategoryListView.as_view()),
-    url(r'^transactions/$', TransactionListView.as_view()),
-    url(
-        regex=r'^transactions/current_month_all$',
-        view=CurrentMonthAllListView.as_view(),
-        name='current-month-spending'
-    ),
-    url(
-        regex=r'^transactions/previous_month_all$',
-        view=PreviousMonthAllListView.as_view(),
-        name='previous-month-spending'
-    ),
-    url(
-        regex=r'^transactions/three_previous_months$',
-        view=ThreePreviousMonthsAllListView.as_view(),
-        name='three-previous-months-spending'
-    ),
+    path('', CurrentMonthAllListView.as_view()),
+    path('categories/', CategoryListView.as_view()),
+    path('transactions/', TransactionListView.as_view()),
+    path('transactions/current_month_all', CurrentMonthAllListView.as_view(), name='current-month-spending'),
+    path('transactions/previous_month_all', PreviousMonthAllListView.as_view(), name='previous-month-spending'),
+    path('transactions/three_previous_months', ThreePreviousMonthsAllListView.as_view(), name='three-previous-months-spending'),
 ]
